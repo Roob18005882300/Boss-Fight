55450452d510b21a52fa2cf7848b12fd2d02b81a
Roob18005882300 <80698127+Roob18005882300@users.noreply.github.com>
2021-04-21 11:45:33 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,178 @@
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    potion.setImage(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . b b b b b b . . 
+        . . . . . . . . . b . . b b . . 
+        . . . . . . . . . b b b b . . . 
+        . . . . . . . . . b d d b . . . 
+        . . . . . . . . b b d d b . . . 
+        . . . . . . . . b d d d b . . . 
+        . . . . . . . b . d . d b . . . 
+        . . . . . . b . d d . . b b . . 
+        . . . . . b b d d . . . d b . . 
+        . . . . b b d d . . . . d b . . 
+        . . . b b d d . . . . . d . b . 
+        . b b . d d . . . . . . d . b . 
+        b b d d . . . . . . . . . d . b 
+        b d d d d d d d d d d d d d . b 
+        b b b b b b b b b b b b b d d d 
+        `)
+    timer.after(5000, function () {
+        potion.setImage(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . b b b 9 9 9 . . 
+            . . . . . . . . . b . . 9 9 . . 
+            . . . . . . . . . b b 9 9 . . . 
+            . . . . . . . . . b d 2 9 . . . 
+            . . . . . . . . b b d 2 9 . . . 
+            . . . . . . . . b d d 2 9 . . . 
+            . . . . . . . b . d . 2 9 . . . 
+            . . . . . . b . d d . . 9 9 . . 
+            . . . . . b b d d . . . 2 9 . . 
+            . . . . b b d d . . . . 2 9 . . 
+            . . . b b d d . . . . . 2 . 9 . 
+            . b b . d d . . . . . . 2 . 9 . 
+            b b d d . . . . . . . . . 2 . 9 
+            b d d d d d d d 2 2 2 2 2 2 . 9 
+            b b b b b b b b 9 b b b b 2 2 2 
+            `)
+    })
+    timer.after(5000, function () {
+        potion.setImage(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . 9 9 9 9 9 9 . . 
+            . . . . . . . . . 9 . . 9 9 . . 
+            . . . . . . . . . 9 9 9 9 . . . 
+            . . . . . . . . . 9 2 2 9 . . . 
+            . . . . . . . . 9 9 2 2 9 . . . 
+            . . . . . . . . 9 2 . 2 9 . . . 
+            . . . . . . . 9 . 2 . 2 9 . . . 
+            . . . . . . 9 . 2 . . . 9 9 . . 
+            . . . . . 9 9 2 . . . . 2 9 . . 
+            . . . . 9 9 2 . . . . . 2 9 . . 
+            . . . 9 9 2 . . . . . . 2 . 9 . 
+            . 9 9 . 2 . . . . . . . 2 . 9 . 
+            9 9 2 2 . . . . . . . . . 2 . 9 
+            9 2 2 2 2 2 2 2 2 2 2 . . 2 . 9 
+            9 9 9 9 9 9 9 9 9 9 9 9 9 2 2 2 
+            `)
+    })
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    SwordSwing.setImage(img`
+        . . . . 1 . . . . . . . . . . . 
+        . . . . . 1 . . . . . . . . . . 
+        . . . . . 1 1 . . . . . . . . . 
+        . . . . . . 1 . . . . . . . . . 
+        . . . . . . 1 1 . . . . . . . . 
+        . . . . . . . 1 . . . . . . . . 
+        . . . . . . . 1 . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `)
+    pause(50)
+    SwordSwing.setImage(img`
+        . . . . 1 . . . . . . . . . . . 
+        . . . . . 1 . . . . . . . . . . 
+        . . . . . 1 1 . . . . . . . . . 
+        . . . . . . 1 1 . . . . . . . . 
+        . . . . . . 1 1 . . . . . . . . 
+        . . . . . . . 1 1 . . . . . . . 
+        . . . . . . . 1 1 . . . . . . . 
+        . . . . . . . . 1 . . . . . . . 
+        . . . . . . . . 1 . . . . . . . 
+        . . . . . . . . 1 . . . . . . . 
+        . . . . . . . . 1 . . . . . . . 
+        . . . . . . . 1 . . . . . . . . 
+        . . . . . . . 1 . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `)
+    pause(50)
+    SwordSwing.setImage(img`
+        . . . . 1 1 . . . . . . . . . . 
+        . . . . . 1 1 1 . . . . . . . . 
+        . . . . . . 1 1 1 . . . . . . . 
+        . . . . . . 1 1 1 . . . . . . . 
+        . . . . . . . 1 1 . . . . . . . 
+        . . . . . . . 1 1 1 . . . . . . 
+        . . . . . . . . 1 1 . . . . . . 
+        . . . . . . . . 1 1 . . . . . . 
+        . . . . . . . . 1 1 . . . . . . 
+        . . . . . . . . . 1 1 . . . . . 
+        . . . . . . . . . 1 1 . . . . . 
+        . . . . . . . . 1 1 1 . . . . . 
+        . . . . . . . 1 1 1 . . . . . . 
+        . . . . . . . 1 1 . . . . . . . 
+        . . . . . . 1 1 . . . . . . . . 
+        . . . . . 1 1 . . . . . . . . . 
+        `)
+    pause(50)
+    SwordSwing.setImage(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . 1 . . . . . . . . 
+        . . . . . . . 1 . . . . . . . . 
+        . . . . . . . . 1 . . . . . . . 
+        . . . . . . . . 1 . . . . . . . 
+        . . . . . . . . 1 . . . . . . . 
+        . . . . . . . . 1 . . . . . . . 
+        . . . . . . . . 1 . . . . . . . 
+        . . . . . . . 1 1 . . . . . . . 
+        . . . . . . 1 1 . . . . . . . . 
+        . . . . . . 1 1 . . . . . . . . 
+        . . . . . . 1 1 . . . . . . . . 
+        . . . . . 1 1 1 . . . . . . . . 
+        . . . . . 1 1 . . . . . . . . . 
+        `)
+    pause(50)
+    SwordSwing.setImage(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . 1 . . . . . . . . 
+        . . . . . . 1 1 . . . . . . . . 
+        . . . . . 1 1 . . . . . . . . . 
+        `)
+    pause(50)
+    SwordSwing.setImage(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `)
+})
 controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
     Sword.setImage(img`
         . . . . . . . . . . . . . . . . 
@@ -35,10 +210,8 @@ controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
         . . . . . . . . . . . . . . . . 
         . . . . . . . . . . . . . . . . 
         `)
+    SwordSwing.setPosition(Player1.x - 16, Player1.y)
 })
-function AttackMelee () {
-	
-}
 controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
     Sword.setImage(img`
         . . . . . . . . . . . . . . . . 
@@ -76,7 +249,18 @@ controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
         . . . . . . . . . . . . . . . . 
         . . . . . . . . . . . . . . . . 
         `)
+    SwordSwing.setPosition(Player1.x + 16, Player1.y)
 })
+info.player2.onLifeZero(function () {
+    scene.cameraShake(8, 5000)
+})
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
+    info.player2.changeLifeBy(-1)
+    scene.cameraShake(5, 500)
+    pause(100)
+})
+let SwordSwing: Sprite = null
+let potion: Sprite = null
 let Sword: Sprite = null
 let Player1: Sprite = null
 scene.setBackgroundImage(img`
@@ -240,7 +424,7 @@ let BossStg1 = sprites.create(img`
     . . f f f f f f f f f f f f . . 
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
-    `, SpriteKind.Player)
+    `, SpriteKind.Enemy)
 BossStg1.setPosition(127, 127)
 Sword = sprites.create(img`
     . . . . . . . . . . . . . . . . 
@@ -260,15 +444,48 @@ Sword = sprites.create(img`
     . . e e . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     `, SpriteKind.Player)
+potion = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . 9 9 9 9 9 9 . . 
+    . . . . . . . . . 9 . . 9 9 . . 
+    . . . . . . . . . 9 9 9 9 . . . 
+    . . . . . . . . . 9 2 2 9 . . . 
+    . . . . . . . . 9 9 2 2 9 . . . 
+    . . . . . . . . 9 2 . 2 9 . . . 
+    . . . . . . . 9 . 2 . 2 9 . . . 
+    . . . . . . 9 . 2 . . . 9 9 . . 
+    . . . . . 9 9 2 . . . . 2 9 . . 
+    . . . . 9 9 2 . . . . . 2 9 . . 
+    . . . 9 9 2 . . . . . . 2 . 9 . 
+    . 9 9 . 2 . . . . . . . 2 . 9 . 
+    9 9 2 2 . . . . . . . . . 2 . 9 
+    9 2 2 2 2 2 2 2 2 2 2 . . 2 . 9 
+    9 9 9 9 9 9 9 9 9 9 9 9 9 2 2 2 
+    `, SpriteKind.Player)
+potion.setFlag(SpriteFlag.Ghost, true)
+potion.setFlag(SpriteFlag.GhostThroughWalls, true)
+SwordSwing = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Projectile)
+info.player2.setLife(3)
 forever(function () {
     Player1.x += controller.dx()
     Player1.y += controller.dy()
-    info.player1.setLife(Player1.x)
-    info.player2.setLife(Player1.y)
     Sword.setPosition(Player1.x, Player1.y)
-})
-forever(function () {
-    if (true) {
-    	
-    }
+    SwordSwing.setPosition(Player1.x + 16, Player1.y)
 })
