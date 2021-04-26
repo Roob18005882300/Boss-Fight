e321fc151098dd06c2e00cb331e30ffda8afd2be
Roob18005882300 <80698127+Roob18005882300@users.noreply.github.com>
2021-04-26 10:38:26 -0400

    Update pxt.json, tilemap.g.jres, tilemap.g.ts

--- tilemap.g.ts
+++ tilemap.g.ts
@@ -44,24 +44,24 @@ namespace myTiles {
 . . . . . . . . . . . . . . . . 
 `, [myTiles.transparency16], TileScale.Sixteen);
             case "level1":
-            case "level1":return tiles.createTilemap(hex`1000100003160404041605050404160404041606150a0e0e0e0e0e0e0e0e0e0e17171713020d1111111111111111111111181707020d1111111111111111111111110f07020d1111111111111111111111110f07150d1111111111111111111111110f13020d111111111b09091d111111110f07020d1111111107212002111111110f07020d11111111071f251e111111110f07020d111111112404041a111111110f07150d1111111111111111111111110f13020d1111111111111111111111110f14020d2210101010102311111111110f07020d0f1b1c09091d0d11111111110f07150c0b071f20211e0c10101010100b1301120908211921010909120909091208`, img`
+            case "level1":return tiles.createTilemap(hex`1000100003160404041605050404160404041606150a0e0e0e0e0e0e0e0e0e0e17171713020d1111111111111111111111181707020d1111111111111111111111110f07020d1111111111111111111111110f07150d1111111111111111111111110f13020d111111111b27271d111111110f07020d1111111126212028111111110f07020d11111111261f2528111111110f07020d111111112429291a111111110f07150d1111111111111111111111110f13020d1111111111111111111111110f14020d2210101010102311111111110f07020d0f1b1c09091d0d11111111110f07150c0b071f20211e0c10101010100b1301120908211921010909120909091208`, img`
 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
 2 . . . . . . . . . . . 2 2 2 2 
 2 . . . . . . . . . . . . 2 2 2 
 2 . . . . . . . . . . . . . . 2 
 2 . . . . . . . . . . . . . . 2 
 2 . . . . . . . . . . . . . . 2 
+2 . . . . . 2 . . 2 . . . . . 2 
 2 . . . . . . . . . . . . . . 2 
 2 . . . . . . . . . . . . . . 2 
-2 . . . . . . . . . . . . . . 2 
-2 . . . . . . . . . . . . . . 2 
+2 . . . . . 2 . . 2 . . . . . 2 
 2 . . . . . . . . . . . . . . 2 
 2 . . . . . . . . . . . . . . 2 
 2 . . . . . . . . . . . . . . 2 
 2 . . 2 2 2 2 2 . . . . . . . 2 
 2 . . 2 2 2 2 2 . . . . . . . 2 
 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
-`, [myTiles.transparency16,sprites.dungeon.greenOuterSouthEast,sprites.dungeon.greenOuterWest1,sprites.dungeon.greenOuterNorthWest,sprites.dungeon.greenOuterNorth0,sprites.dungeon.greenOuterNorth1,sprites.dungeon.greenOuterNorthEast,sprites.dungeon.greenOuterEast0,sprites.dungeon.greenOuterSouthWest,sprites.dungeon.greenOuterSouth1,sprites.dungeon.darkGroundNorthWest0,sprites.dungeon.darkGroundSouthEast0,sprites.dungeon.darkGroundSouthWest0,sprites.dungeon.darkGroundWest,sprites.dungeon.darkGroundNorth,sprites.dungeon.darkGroundEast,sprites.dungeon.darkGroundSouth,sprites.dungeon.darkGroundCenter,sprites.dungeon.greenOuterSouth2,sprites.dungeon.greenOuterEast2,sprites.dungeon.greenOuterEast1,sprites.dungeon.greenOuterWest2,sprites.dungeon.greenOuterNorth2,sprites.dungeon.stairLadder,sprites.dungeon.chestClosed,sprites.dungeon.floorDarkDiamond,sprites.dungeon.greenInnerSouthEast,sprites.dungeon.greenInnerNorthWest,sprites.dungeon.greenOuterSouth0,sprites.dungeon.greenInnerNorthEast,sprites.dungeon.greenOuterWest0,sprites.dungeon.floorDark4,sprites.dungeon.floorDark1,sprites.dungeon.floorDark0,sprites.dungeon.darkGroundNorthWest1,sprites.dungeon.darkGroundNorthEast1,sprites.dungeon.greenInnerSouthWest,sprites.dungeon.floorDark5], TileScale.Sixteen);
+`, [myTiles.transparency16,sprites.dungeon.greenOuterSouthEast,sprites.dungeon.greenOuterWest1,sprites.dungeon.greenOuterNorthWest,sprites.dungeon.greenOuterNorth0,sprites.dungeon.greenOuterNorth1,sprites.dungeon.greenOuterNorthEast,sprites.dungeon.greenOuterEast0,sprites.dungeon.greenOuterSouthWest,sprites.dungeon.greenOuterSouth1,sprites.dungeon.darkGroundNorthWest0,sprites.dungeon.darkGroundSouthEast0,sprites.dungeon.darkGroundSouthWest0,sprites.dungeon.darkGroundWest,sprites.dungeon.darkGroundNorth,sprites.dungeon.darkGroundEast,sprites.dungeon.darkGroundSouth,sprites.dungeon.darkGroundCenter,sprites.dungeon.greenOuterSouth2,sprites.dungeon.greenOuterEast2,sprites.dungeon.greenOuterEast1,sprites.dungeon.greenOuterWest2,sprites.dungeon.greenOuterNorth2,sprites.dungeon.stairLadder,sprites.dungeon.chestClosed,sprites.dungeon.floorDarkDiamond,sprites.dungeon.greenInnerSouthEast,sprites.dungeon.greenInnerNorthWest,sprites.dungeon.greenOuterSouth0,sprites.dungeon.greenInnerNorthEast,sprites.dungeon.greenOuterWest0,sprites.dungeon.floorDark4,sprites.dungeon.floorDark1,sprites.dungeon.floorDark0,sprites.dungeon.darkGroundNorthWest1,sprites.dungeon.darkGroundNorthEast1,sprites.dungeon.greenInnerSouthWest,sprites.dungeon.floorDark5,sprites.dungeon.stairWest,sprites.dungeon.stairSouth,sprites.dungeon.stairEast,sprites.dungeon.stairNorth], TileScale.Sixteen);
         }
         return null;
     })
